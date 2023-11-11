d01e007f14dc057c75e77b9d7b9493fb9ab7316f
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-11-11 18:40:00 +0200

    .

--- src/main/java/onlineshop/model/entity/CartEntity.java
+++ src/main/java/onlineshop/model/entity/CartEntity.java
@@ -1,12 +1,12 @@
 package onlineshop.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Setter
 @Getter
@@ -14,9 +14,9 @@ import lombok.Setter;
 @AllArgsConstructor
 @Entity
 @Table(name = "carts")
-public class Cart extends BaseEntity{
-    @Column
-    private Product product;
-    @Column
-    private int quantity;
+public class CartEntity extends BaseEntity{
+    @ManyToOne
+    private User user;
+    @OneToMany
+    private List<Product> products;
 }
