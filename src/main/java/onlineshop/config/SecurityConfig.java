864bb35801ab52a759b7d25b2b8742046d88cebc
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-12-29 20:42:07 +0200

    .

--- src/main/java/onlineshop/config/SecurityConfig.java
+++ src/main/java/onlineshop/config/SecurityConfig.java
@@ -3,16 +3,24 @@ package onlineshop.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(a -> a.anyRequest().permitAll());
+
+        http.authorizeHttpRequests((auth) ->
+                auth
+                        .requestMatchers("/css/**", "/login", "/users/login", "/users/register", "/")
+                        .permitAll()
+                        .requestMatchers("/products/add").hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated());
+
         http.csrf(AbstractHttpConfigurer::disable);
         http.cors(AbstractHttpConfigurer::disable);
         return http.build();
