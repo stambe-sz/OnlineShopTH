f2c1ec4d4335f8ee4cd27f957ae72f767bbe8290
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2024-01-03 00:29:43 +0200

    .

--- src/main/java/onlineshop/config/SecurityConfig.java
+++ src/main/java/onlineshop/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package onlineshop.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,14 +13,20 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests((auth) ->
+        http.authorizeHttpRequests(auth ->
                 auth
-                        .requestMatchers("/css/**", "/login", "/users/login", "/users/register", "/")
-                        .permitAll()
-                        .requestMatchers("/products/add").hasRole("ADMIN")
-                        .anyRequest()
-                        .authenticated());
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/","/users/login","/users/register").permitAll()
+                        .anyRequest().authenticated()
+        );
+
+//        http.authorizeHttpRequests((auth) ->
+//                auth
+//                        .requestMatchers("/css/**", "/login", "/users/login", "/users/register", "/")
+//                        .permitAll()
+//                        .requestMatchers("/products/add").hasRole("ADMIN")
+//                        .anyRequest()
+//                        .authenticated());
 
         http.csrf(AbstractHttpConfigurer::disable);
         http.cors(AbstractHttpConfigurer::disable);
