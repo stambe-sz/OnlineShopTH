5f00d64f4b1ce7da909702a232497a5fd1523515
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2024-06-21 14:17:04 +0300

    .

--- src/main/java/onlineshop/config/SecurityConfig.java
+++ src/main/java/onlineshop/config/SecurityConfig.java
@@ -4,20 +4,30 @@ import lombok.RequiredArgsConstructor;
 import onlineshop.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalAuthentication
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfiguration {
     private final UserService userService;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -76,9 +86,18 @@ public class SecurityConfig {
         return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(User.withUsername(userService.));
-//    }
+    @Bean
+    public UserDetailsService userDetailsService(AuthenticationManagerBuilder auth) {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        ArrayList<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
+
+        SimpleGrantedAuthority role1 = new SimpleGrantedAuthority("ADMIN");
+        SimpleGrantedAuthority role2 = new SimpleGrantedAuthority("USER");
+
+        roles.add(role1);
+        roles.add(role2);
+
+        userDetailsManager.createUser(User.withUsername(userService.getUserByUsername()));
+    }
+    
 }
