389b4ccf139ea13bb2cb4e309f1e3e9be461741a
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2024-07-20 17:41:01 +0300

    cart controller

--- src/main/java/onlineshop/web/controllers/CartController.java
+++ src/main/java/onlineshop/web/controllers/CartController.java
@@ -2,9 +2,13 @@ package onlineshop.web.controllers;
 
 import lombok.RequiredArgsConstructor;
 import onlineshop.model.binding.CartItemBindingModel;
+import onlineshop.model.entity.Cart;
 import onlineshop.model.service.CartItemServiceModel;
+import onlineshop.model.service.UserServiceModel;
 import onlineshop.model.view.ProductViewModel;
 import onlineshop.service.CartService;
+import onlineshop.service.UserService;
+import onlineshop.tools.Tools;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,17 +27,34 @@ public class CartController {
 
     private final CartService cartService;
     private final ModelMapper modelMapper;
+    private final UserService userService;
+
 
     @GetMapping("")
-    public ModelAndView getCart(ModelAndView model) throws Exception {
-        List<ProductViewModel> allProducts = this.cartService
-                .getCartItems()
-                .stream()
-                .map(e -> this.modelMapper.map(e, ProductViewModel.class))
-                .collect(Collectors.toList());
-        model.setViewName("cart");
-        model.addObject("allProducts", allProducts);
+    public ModelAndView getCart(ModelAndView model, Tools tools) throws Exception {
+        String username = tools.getLoggedUser();
+        UserServiceModel foundUser = userService.getUserByUsername(username);
+
+        if (username != null && foundUser.getCart() != null){
+            List<ProductViewModel> allProducts = foundUser.getCart()
+                    .getCartItems().stream()
+                    .map(e -> this.modelMapper.map(e,ProductViewModel.class))
+                    .collect(Collectors.toList());
+            model.setViewName("cart");
+            model.addObject("allProducts",allProducts);
+        }else {
+            model.addObject("cart", new Cart());
+        }
         return model;
+
+//            List<ProductViewModel> allProducts = this.cartService
+//                    .getCartItems()
+//                    .stream()
+//                    .map(e -> this.modelMapper.map(e, ProductViewModel.class))
+//                    .collect(Collectors.toList());
+//            model.setViewName("cart");
+//            model.addObject("allProducts", allProducts);
+//            return model;
     }
     @GetMapping("/cart/checkout")
     public String cartCheckout(@ModelAttribute String username) {
@@ -46,4 +67,5 @@ public class CartController {
                 this.modelMapper.map(cibm, CartItemServiceModel.class)
         );
     }
+
 }
