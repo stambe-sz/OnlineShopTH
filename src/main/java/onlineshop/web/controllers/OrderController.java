10e08d3ab347d345b8c63c698577db8e26e8f95a
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2024-07-23 21:53:11 +0300

    .

--- src/main/java/onlineshop/web/controllers/OrderController.java
+++ src/main/java/onlineshop/web/controllers/OrderController.java
@@ -1,9 +1,14 @@
 package onlineshop.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping()
 public class OrderController {
+    @GetMapping("/order")
+    public String getHomePage() {
+        return "order";
+    }
 }
