187c23de55bd07cd6198f1294a4093471f5cde79
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2024-07-27 21:59:30 +0300

    webconfig

--- src/main/java/onlineshop/config/WebConfig.java
+++ src/main/java/onlineshop/config/WebConfig.java
@@ -1,9 +1,20 @@
 package onlineshop.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final LocaleChangeInterceptor localeChangeInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(localeChangeInterceptor);
+    }
 }
