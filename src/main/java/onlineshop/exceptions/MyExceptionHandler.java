5d00857e59d8857029e9936a8dd2e3e5ea36cb34
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2024-08-04 18:36:07 +0300

    error

--- src/main/java/onlineshop/exceptions/MyExceptionHandler.java
+++ src/main/java/onlineshop/exceptions/MyExceptionHandler.java
@@ -1,9 +1,26 @@
 package onlineshop.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class MyExceptionHandler {
 
-    //TODO...
+    private static final Logger logger = LoggerFactory.getLogger(MyExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception ex, Model model) {
+        logger.error("Exception occurred: ", ex);
+        model.addAttribute("message", ex.getMessage());
+        model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        model.addAttribute("page", "error");
+        model.addAttribute("pageTitle", "ERROR_PAGE");
+        return "error";
+    }
 }
